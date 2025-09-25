701a06309390b2b1e4a786ab4a092eac880137db
JSLee <ljs1990@gmail.com>
2025-09-25 16:09:20 +0900

    1

--- test1/src/main/java/com/example/test1/dao/UserService.java
+++ test1/src/main/java/com/example/test1/dao/UserService.java
@@ -23,12 +23,17 @@ public class UserService {
 	public HashMap<String, Object> userLogin(HashMap<String, Object> map) {
 		// TODO Auto-generated method stub
 		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+		
 		System.out.println("service => " + map);
 		User user = userMapper.userLogin(map);
 		if(user != null) {
 			System.out.println(user.getName());
 			System.out.println(user.getNickName());
 		}
+		
+		resultMap.put("info", user);
+		resultMap.put("result", "success");
+		
 		return resultMap;
 	}
 	
